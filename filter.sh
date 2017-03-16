2daf20391e8ac757027f1e569d408344dd853cc7
Dandong Yin <dyin4@illinois.edu>
2017-03-16 16:47:37 -0500

    Added custom choices of pre-defined bbox

--- filter.sh
+++ filter.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 if [[ $# == 1 ]]
 then
+	# no bbox file specified, just filter for geo-tagged tweets
 	export ALL_GEO=1 
         twtfile=$1
 else
 	bbox=$1
+	if [[ -f "$bbox" ]]
+	then
+		# specific bbox file provided
+		bboxfile=$bbox
+	else
+	        if [[ -f "~/bin/bbox/${bbox}.bbox" ]]
+		then 
+			# pre-defined bbox files can be called with names
+			bboxfile=~/bin/bbox
+		else
+			echo "No such bbox file!"
+			exit 1
+		fi
+	fi
+
 	set -o allexport
-	source $bbox
+	source ${bboxfile}
 	set +o allexport
         twtfile=$2
 fi
